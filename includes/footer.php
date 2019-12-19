fe084728a99cd820104f7dd55d2a612ca5482cb4
Enzo <dorosario.enzo@protonmail.com>
2019-12-19 15:32:51 +0100

    include php files

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,11 @@ $date = date('Y');
 
 <footer>
     <p>&copy; <?= $date ?> NWS Corporation</p>
+    <address>
+        72 rue de la république<br />
+        76140 Le Petit-Quevilly<br />
+        <a href="tel:+33232023247">Call me babe!</a>
+    </address>
 </footer>
 </body>
 
