6a1fa47786c37de9cd450ee31bb0ff126412d859
Enzo <dorosario.enzo@protonmail.com>
2019-12-19 16:23:49 +0100

    use reset instead of normalize

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Inscription</a></li>
-            <li><a href="#">Mentions Légales</a></li>
+            <li><a href="index.php?page=accueil">Accueil</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+            <li><a href="index.php?page=inscription">Inscription</a></li>
+            <li><a href="index.php?page=mentions">Mentions Légales</a></li>
         </ul>
     </nav>
 </header>
